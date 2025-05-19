e1a3e09e196cdc52a943e7903ea216931fb37503
DevyanshP <22BCAE19@kristujayanti.com>
2025-05-19 13:15:05 +0530

    chang

--- add.c
+++ add.c
@@ -15,7 +15,7 @@ int main() {
     
     sum = num1 + num2 + num3;
     
-    printf("The sum of %d and %d is %d.", num1, num2, sum);
+    printf("The sum of %d and %d is %d.", num1, num2,num3);
     
     return 0;
 }
